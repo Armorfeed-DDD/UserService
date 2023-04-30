be923406b387f8d0e43c3b3905d33b818546ad15
De La Mar <delamarzp@gmail.com>
2023-04-29 20:07:26 -0500

    feat: add user service

---
+++ src/main/java/com/amorfeed/api/userservice/service/UserService.java
@@ -0,0 +1,13 @@
+package com.amorfeed.api.userservice.service;
+
+import com.amorfeed.api.userservice.entity.User;
+import com.amorfeed.api.userservice.resource.RegisterResource;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface UserService {
+    ResponseEntity<?> register(RegisterResource request);
+
+    List<User> getAll();
+}
