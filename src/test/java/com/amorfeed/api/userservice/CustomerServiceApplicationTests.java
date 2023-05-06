9d30c7abb0bfcca3dfabd5b8ebdb860d287d73c1
FranchescoSoto <u201821078@upc.edu.pe>
2023-05-06 01:40:53 -0500

    Revert "feature: add tests"
    This reverts commit aab8d9fb95c55a7ac77188955f0350c8d7fc4a65.

--- src/test/java/com/amorfeed/api/userservice/CustomerServiceApplicationTests.java
+++ src/test/java/com/amorfeed/api/userservice/CustomerServiceApplicationTests.java
@@ -1,61 +1,13 @@
 package com.amorfeed.api.userservice;
 
-import com.amorfeed.api.userservice.entity.User;
-import com.amorfeed.api.userservice.repository.UserRepository;
-import com.amorfeed.api.userservice.resource.ChangeEmailResource;
-import com.amorfeed.api.userservice.resource.ChangePasswordResource;
-import com.amorfeed.api.userservice.service.UserService;
-import com.amorfeed.api.userservice.service.impl.UserImpl;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
-
-import static org.mockito.BDDMockito.given;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class CustomerServiceApplicationTests {
 
-    @Mock
-    private UserRepository userRepository;
-
-    @InjectMocks
-    private UserImpl userService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
     @Test
-    public void changeEmail() {
-        User user=new User(1L,"Edery","edery@hotmail.com","12345");
-        userRepository.save(user);
-        String newEmail="edery77@hotmail.com";
-        ChangeEmailResource changeEmailResource=new ChangeEmailResource(user.getEmail(),newEmail);
-        userService.changeEmail(changeEmailResource);
-        Optional<User> userExpected= userRepository.findByEmail(newEmail);
-        assertThat(userExpected.get()).isNotNull();
-        assertEquals(userExpected.get().getEmail(),newEmail);
+    void contextLoads() {
     }
 
-    @Test
-    public void changePassword(){
-        User user=new User(1L,"Edery","edery@hotmail.com","12345");
-        userRepository.save(user);
-        String newPassword="54321";
-        ChangePasswordResource changePasswordResource=new ChangePasswordResource(user.getEmail(),user.getPassword(),newPassword);
-        userService.changePassword(changePasswordResource);
-        Optional<User> userExpected=userRepository.findByEmail(user.getEmail());
-        assertThat(userExpected.get()).isNotNull();
-        assertThat(passwordEncoder.matches(newPassword,userExpected.get().getPassword())).isTrue();
-    }
 }
